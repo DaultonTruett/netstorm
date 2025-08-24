2eb0e5fd17970f953964e9a1c4dc031e05fb6d83
Daulton Truett <dtruett45@gmail.com>
2025-08-23 18:29:35 -0600

    Modified AttackSession model to include source and target latitude and longitude along with an ATTACK_TYPES array of tuples.

--- backend/attacks/models.py
+++ backend/attacks/models.py
@@ -2,9 +2,25 @@ from django.db import models
 
 # Create your models here.
 class AttackSession(models.Model):
+    ATTACK_TYPES = [
+        ('DDoS', 'DDoS'), 
+        ('Phishing', 'Phishing'),
+        ('Malware', 'Malware'),
+        ('Ransomware', 'Ransomware'),
+        ('SQL Injection', 'SQL Injection'),
+        ('Exploit', 'Exploit'),
+        ('Worm', 'Worm')
+    ]
+    
     source_ip = models.GenericIPAddressField()
+    source_lat = models.FloatField(null=True, blank=True)
+    source_lon = models.FloatField(null=True, blank=True)
+    
     target_ip = models.GenericIPAddressField()
-    attack_type = models.CharField(max_length=100)
+    target_lat = models.FloatField(null=True, blank=True)
+    target_lon = models.FloatField(null=True, blank=True)
+    
+    attack_type = models.CharField(max_length=100, choices=ATTACK_TYPES)
     current_phase = models.CharField(max_length=100, default='Recon')
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(null=True, blank=True)
